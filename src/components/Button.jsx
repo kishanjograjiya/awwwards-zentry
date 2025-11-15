ea7f4752cc34e3750f0cf70b910f5f35947bccb1
Kishan Jograjiya <kishanjograjiya.dev@gmail.com>
2025-11-15 22:52:51 +0530

    Enhance UI components: update Button animations, add IDs to Features, Hero, and Story sections, and refactor Footer social links

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -10,9 +10,16 @@ const Button = ({ id, title, rightIcon, leftIcon, containerClass }) => {
       )}
     >
       {leftIcon}
+
       <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
-        <div>{title}</div>
+        <div className="translate-y-0 skew-y-0 transition duration-500 group-hover:translate-y-[-160%] group-hover:skew-y-12">
+          {title}
+        </div>
+        <div className="absolute translate-y-[164%] skew-y-12 transition duration-500 group-hover:translate-y-0 group-hover:skew-y-0">
+          {title}
+        </div>
       </span>
+
       {rightIcon}
     </button>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -76,7 +76,7 @@ const Hero = () => {
   const getVideoSource = (index) => `/videos/hero-${index}.mp4`;
 
   return (
-    <div className="relative h-dvh w-screen overflow-x-hidden">
+    <div id="nexus" className="relative h-dvh w-screen overflow-x-hidden">
       {isLoading && (
         <div className="flex-center absolute z-100 h-dvh w-screen overflow-hidden bg-violet-50">
           <div className="three-body">
